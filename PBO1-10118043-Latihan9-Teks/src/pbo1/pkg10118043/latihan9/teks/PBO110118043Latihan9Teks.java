60cdaca53e572e8223be21fa5a3a9de7de4b8bd7
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 15:16:29 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan9-Teks/src/pbo1/pkg10118043/latihan9/teks/PBO110118043Latihan9Teks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118043.latihan9.teks;
+
+/**
+ *
+ * @author user
+ */
+public class PBO110118043Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        
+        System.out.println("Hasil" +newline+ "Char" +tab+"Berisi " +c);
+    }
+    
+}
